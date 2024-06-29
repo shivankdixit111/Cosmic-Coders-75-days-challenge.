f21b1cb6ed2d9477e4df3d3ed2b31495628a7569
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-06-29 13:20:58 +0530

    Time: 50 ms (25.88%), Space: 51.4 MB (11.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int rec(int amount, vector<int>& coins, vector<int> &dp) {
-        int n=coins.size();
-        if(dp[amount]!=1e9)  return dp[amount];
+    // int rec(int amount, vector<int>& coins, vector<int> &dp) {
+    //     int n=coins.size();
+    //     if(dp[amount]!=1e9)  return dp[amount];
 
-        if(amount<=0) return 0;
+    //     if(amount<=0) return 0;
 
-        int ans=1e7;
-        for(int i=0;i<n;i++) {
-            if(amount>=coins[i]) {
-                ans=min(ans,rec(amount-coins[i],coins,dp)+1);
-            }
+    //     int ans=1e7;
+    //     for(int i=0;i<n;i++) {
+    //         if(amount>=coins[i]) {
+    //             ans=min(ans,rec(amount-coins[i],coins,dp)+1);
+    //         }
+    //     }
+    //     return dp[amount]=ans;
+    // }
+
+    int rec(int idx,int amount,vector<int> &coins, vector<vector<int>> &dp) {
+         
+        if(idx==0) {
+             if(amount % coins[idx]==0) return amount / coins[idx];
+            return 1e7;
         }
-        return dp[amount]=ans;
+
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+
+        int nottake=rec(idx-1,amount,coins,dp);
+        int take=1e9;
+        if(coins[idx]<=amount) {
+            take=rec(idx,amount-coins[idx],coins,dp)+1;
+        }
+
+        return dp[idx][amount]=min(take,nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
 
-        vector<int> dp(100004,1e9);
+        // vector<int> dp(100004,1e9);
         // dp[0]=0;
 
         // for(int i=0;i<n;i++) {
@@ -29,7 +47,12 @@ public:
         // } 
 
         // return dp[amount]==1e9?-1:dp[amount];  
-        int ans= rec(amount,coins,dp);
+        // int ans= rec(amount,coins,dp);
+        // return ans==1e7?-1:ans;
+
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+
+        int ans=rec(n-1,amount,coins,dp);
         return ans==1e7?-1:ans;
     } 
 };
