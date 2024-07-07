ca5b9223d0d96a392b3655ceecc97dd79bafa509
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-07-07 18:08:15 +0530

    Time: 20 ms (52.53%), Space: 19.6 MB (72.5%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -7,7 +7,7 @@ public:
         if(i<0) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
-        
+
         int res=0;
         if(s[i]==t[j]) {
            res=rec(i-1,j-1,s,t,dp) + rec(i-1,j,s,t,dp);
@@ -19,7 +19,23 @@ public:
     }
     int numDistinct(string s, string t) {
         int n=s.length(),m=t.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return rec(n-1,m-1,s,t,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // return rec(n-1,m-1,s,t,dp);
+        int mod=1e9+7;
+        
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                int ans=0;
+                if(s[i-1]==t[j-1]) {
+                    ans=(dp[i-1][j-1]+dp[i-1][j])%mod;
+                } else {
+                    ans=dp[i-1][j];
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[n][m];
     }
 };
