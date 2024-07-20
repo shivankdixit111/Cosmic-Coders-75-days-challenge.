539b7fed46b1c65e424c35061edd18fd1a645e4f
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-07-20 22:36:07 +0530

    Time: 282 ms (5.03%), Space: 29.2 MB (5.91%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size();
+
+        vector<vector<int>> psum(n+1,vector<int>(m+1,0));
+
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                psum[i][j] = psum[i-1][j] + psum[i][j-1] - psum[i-1][j-1] +(matrix[i-1][j-1]==1);
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(matrix[i][j]==1) cnt++;
+            }
+        }
+ 
+        for(int sz=2;sz<=min(n,m);sz++) {
+            for(int i=1;i<=n;i++) {
+                for(int j=1;j<=m;j++) {
+                   int x2=i+sz-1,y2=j+sz-1;
+                   if(x2>n or y2>m) break;
+                   int sum = psum[x2][y2] - psum[x2][j-1] - psum[i-1][y2] + psum[i-1][j-1];
+                   if(sum==(y2-j+1)*(x2-i+1)) {
+                      cnt++;
+                   }
+                } 
+            }
+        }
+            return cnt;
+        }
+    };
