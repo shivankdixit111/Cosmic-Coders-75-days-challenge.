22c07c9c348be7b5e0ac20ab5b506a6aa094c75d
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-07-28 13:32:08 +0530

    Time: 18 ms (58.64%), Space: 16.3 MB (48.72%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -13,16 +13,13 @@ public:
     }
     bool isBipartite(vector<vector<int>>& graph) { 
          int n=graph.size();
-        
- 
-         vector<int> color(101,-1);
 
+         vector<int> color(101,-1);
          for(int i=0;i<n;i++) {
             if(color[i]==-1) {
                 if(dfs(i,graph,0,color)==false) return false;
             }
          }
-
-        return true;
+         return true;
     }
 };
