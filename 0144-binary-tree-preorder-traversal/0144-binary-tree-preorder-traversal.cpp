7229c201749472aff270974d7bac6741c47417e3
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-08-19 20:38:59 +0530

    Time: 4 ms (32.02%), Space: 9.9 MB (77.37%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,20 +12,31 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
-        vector<int> ans;
-        if(root==NULL) return {};
-
-        st.push(root);
-
-        while(st.size()) {
-            TreeNode* node = st.top();
-            st.pop();
-            ans.push_back(node->val);
-
-            if(node->right) st.push(node->right);
-            if(node->left) st.push(node->left);
-        }
-        return ans;
+               vector<int> preorder;
+         // MORRIS preorder  O(1) space complexity
+         while(root!=NULL) {
+            if(root->left==NULL) {
+                 preorder.push_back(root->val);
+                 root = root->right;
+            } else {
+                TreeNode* prev = root->left; 
+                // make a thread by going(prev copy goes) to left of root till the rightmost 
+                // node of left of root node
+                // and delete the thread while returning from it
+                while(prev->right and prev->right!=root) {
+                    prev = prev->right;
+                }
+                if(prev->right==NULL) {
+                    prev->right = root; // thread connected
+                    preorder.push_back(root->val);
+                    root = root->left; //explore further left portion
+                } else {
+                    prev->right = NULL; // delete the thread as everything is explored to that node 
+                    // now explore the right portion
+                    root= root->right;
+                }
+            }
+         }
+         return preorder;
     }
 };
