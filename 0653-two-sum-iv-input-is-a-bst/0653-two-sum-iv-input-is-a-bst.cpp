a234895b03f74564b2b4216ce8f2cda6814b5f2f
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-08-30 12:54:01 +0530

    Time: 23 ms (86.47%), Space: 35.1 MB (98.51%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -23,8 +23,50 @@ public:
          return left or right;
          
     }
+    TreeNode* largest( TreeNode* root) {
+        if(root->right==NULL) return root;
+        return largest(root->right);
+    }
+    TreeNode* smallest( TreeNode* root) {
+        if(root->left==NULL) return root;
+        return smallest(root->left);
+    }
+    TreeNode* predecessor( TreeNode* root, TreeNode* right, TreeNode* pd) {
+        if(!root) return pd;
+
+        if(root->val < right->val) {
+            pd = root;
+            return predecessor( root->right, right, pd);
+        } else {
+            return predecessor( root->left, right, pd);
+        }
+    }
+     TreeNode* successor( TreeNode* root, TreeNode* right, TreeNode* sd) {
+        if(!root) return sd;
+
+        if(root->val > right->val) {
+            sd = root;
+            return successor( root->left, right,sd);
+        } else {
+            return successor( root->right, right,sd);
+        }
+    }
     bool findTarget(TreeNode* root, int k) {
-          unordered_map<int,int> mp;
-          return fun(root,k,mp);
+        unordered_map<int,int> mp;  // tc- O(n)  sc- O(n)
+        //   return fun(root,k,mp);
+
+        TreeNode* left = smallest(root);
+        TreeNode* right = largest(root);
+
+        while(left!=right) {
+            int sum = left->val + right->val;
+            if(sum==k) return 1;
+            if(sum>k) {
+                right = predecessor(root,right,NULL);
+            } else {
+                left = successor(root,left, NULL);
+            }
+        }
+        return 0;
     }
 };
