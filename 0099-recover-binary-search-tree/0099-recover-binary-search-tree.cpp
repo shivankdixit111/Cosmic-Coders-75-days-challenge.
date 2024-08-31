5d5af16deeccca279f811a14ca62f608bc3cb1eb
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-08-31 09:23:14 +0530

    Time: 12 ms (94.11%), Space: 60.7 MB (72%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -13,29 +13,65 @@ class Solution {
 public:
     TreeNode* firstElement = NULL;
     TreeNode* secondElement = NULL;
-    TreeNode* prevNode = new TreeNode(INT_MIN);
-    
-    void traverse( TreeNode* root) {
-        if(!root) return;
+    TreeNode* prevNode = NULL;
 
-        traverse(root->left); 
+    // void traverse( TreeNode* root) {
+    //     if(!root) return;
+
+    //     traverse(root->left); 
  
-        if(firstElement==NULL and root->val < prevNode->val) { // cause BST is always in sorted order so prev can never be greater
-            firstElement = prevNode;
-        }
-        if(firstElement!=NULL and root->val < prevNode->val) {
-            secondElement = root;
-        }
+    //     if(firstElement==NULL and root->val < prevNode->val) { // cause BST is always in sorted order so prev can never be greater
+    //         firstElement = prevNode;
+    //     }
+    //     if(firstElement!=NULL and root->val < prevNode->val) {
+    //         secondElement = root;
+    //     }
         
-        prevNode = root;
+    //     prevNode = root;
 
-        traverse(root->right);
-    }
+    //     traverse(root->right);
+    // }
     void recoverTree(TreeNode* root) {
-         traverse(root); 
-         cout<< firstElement->val <<" "<< secondElement->val <<endl;
 
-         swap(firstElement->val,secondElement->val); 
+        //  traverse(root); 
+        //  cout<< firstElement->val <<" "<< secondElement->val <<endl;
+        //  swap(firstElement->val,secondElement->val);
+
+        // Follow Up -> O(1) space using Morris Inorder Traversal
+
+        while(root) {
+            if(prevNode!=NULL and prevNode->val > root->val) {  
+                if(firstElement==NULL) firstElement = prevNode;
+                if(firstElement!=NULL) secondElement = root;
+            }
+            if(root->left==NULL) {
+                // take value
+                prevNode = root;
+                root= root->right;  
+            } else {
+                //creat thread by going to rightmost node of left subtree 
+                // delete after taking value
+                TreeNode* prev = root->left;
+                while(prev->right!=NULL and prev->right!=root) prev=prev->right;
+
+                if(prev->right==NULL) {
+                    // creat thread to root for convience to go to root while returning
+                    // as we are using no recursion
+                    prev->right = root;
+                    //explore left part
+                    root= root->left;
+                } else {
+                    //there is a thread delete it
+                    prev->right = NULL;
+                    // take value  
+                    prevNode = root;
+                    root = root->right;
+                }
+            }
+        }
+
+        swap( firstElement->val , secondElement->val );
+
          
     }
 };
