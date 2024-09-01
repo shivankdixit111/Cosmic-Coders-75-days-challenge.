bc809726371d7ea64e9ad079e4eb45e0de756c74
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-09-01 17:07:56 +0530

    Time: 611 ms (40.11%), Space: 182.9 MB (59.41%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,64 +1,61 @@
-struct TrieNode {
-    TrieNode* links[2];
-    
-    TrieNode() {
-        links[0]=NULL;
-        links[1]=NULL;
-    }
+  struct TrieNode {
+     TrieNode* arr[2]; 
+     TrieNode() {
+        arr[0]=NULL;
+        arr[1]=NULL;
+     }
+  };
+ 
 
-    bool containsKey(int bit) {
-        return links[bit]!=NULL;
-    }
-    void put(TrieNode* node, int bit) {
-        links[bit] = node;
-    }
-    TrieNode* getbit(int bit) {
-        return links[bit];
-    }
-};
-    
-TrieNode* root=NULL;
-void insert(int num) {
-    TrieNode* curr = root;
-    for(int i=31;i>=0;i--) {
-        int b=-1;
-        if((1<<i)&num) b=1;
-        else b=0;
-        if(!curr->containsKey(b)) {
-             curr->put(new TrieNode(), b);
-        }
-        curr=curr->getbit(b);
-    }
-}
-int getMax(int num) {
-    TrieNode* curr = root;
-    int maxNum = num;
-    for(int i=31;i>=0;i--) {
-        int b=-1;
-        if((1<<i)&num) b=1;
-        else b=0; 
-        if(curr->containsKey(!b)) {  //opposite key
-            maxNum = maxNum | (1<<i);
-            curr = curr-> getbit(1-b);
-        } else {
-            if(b==1) 
-                 maxNum = maxNum & (maxNum-(1<<i));  //making it unset
-            curr = curr-> getbit(b);
-        }
-    }
-    return maxNum;
-}
+  void insert(TrieNode* root, int val) {
+       TrieNode* temp = root; 
+       
+       for(int i=31;i>=0;i--) {
+           // checking ith bit is set or not
+           int bit = ((1<<i) & val) ? 1: 0;
+
+           if(temp->arr[bit]==NULL) {
+              temp-> arr[bit] = new TrieNode();
+           }
+        //    cout<<i<<" - "<<bit<<" ";
+           temp = temp-> arr[bit];
+       }
+    //    cout<<endl;
+  }
+ 
+  int get_max_xor_of_two(TrieNode* root, int val) {
+       TrieNode* temp = root;
+        int num=0;
+       
+       for(int i=31;i>=0;i--) {
+           // checking ith bit is set or not
+           int bit = ((1<<i) & val) ? 1: 0;
+           
+           // for maximum xor find the opposite bit
+           if(temp->arr[1-bit]!=NULL) {
+
+              num += (1-bit) ? (1<<i) : 0;
+              temp = temp-> arr[1-bit];
+
+           } else if(temp->arr[bit]!=NULL) {
+              // if opposite bit not present go to same bit
+              num += bit ? (1<<i) : 0; 
+              temp = temp-> arr[bit];
+           }
+       }
+       return num ^ val;
+  }
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) { 
-        root = new TrieNode();
+        TrieNode* root = new TrieNode();
         for(auto it:nums) {
-            insert(it);
+            insert(root,it);
         }
         int maxi = INT_MIN;
-        for(auto it:nums) {
-            cout<<getMax(it)<<" ";
-            maxi = max(maxi, getMax(it));
+
+        for(auto it:nums) { 
+            maxi = max(maxi, get_max_xor_of_two(root,it));
         }
         return maxi;
     }
