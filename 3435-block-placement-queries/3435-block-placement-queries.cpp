5e4a967de6db50d85a6f9a604d71fe1ec26083eb
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-09-21 19:56:17 +0530

    Time: 1360 ms (24.74%), Space: 381.1 MB (13.98%) - LeetHub

---
+++ 3435-block-placement-queries/3435-block-placement-queries.cpp
@@ -0,0 +1,84 @@
+
+class Solution {
+public:
+   
+vector<long long> Segtree;  
+
+void pointupdate(int low, int high, int index, int pos, int value)
+{ // time complexity--> O(log n)
+    if (low == high)
+    {
+        Segtree[index] = value;
+        return;
+    }
+    int mid = (low + high) >> 1;
+
+    if (pos >= low and pos <= mid)
+        pointupdate(low, mid, 2 * index + 1, pos, value);
+    else
+        pointupdate(mid + 1, high, 2 * index + 2, pos, value);
+
+    Segtree[index] = max(Segtree[2 * index + 1] , Segtree[2 * index + 2]);
+}
+
+int rangeMax(int low, int high, int index, int l, int r)
+{ // time complexity--> O(log n)
+    // complete overlap       l........low.........high........r
+    if (low == l and high == r)
+        return Segtree[index];
+
+    // disjoint
+    if (l > r)
+        return INT_MIN;
+
+    int mid = (low + high) >> 1;
+    int leftanswer = rangeMax(low, mid, 2 * index + 1, l, min(mid,r));
+    int rightanswer = rangeMax(mid + 1, high, 2 * index + 2, max(l,mid+1), r);
+
+    //   cout<<index<<" "<<leftanswer<<" "<<rightanswer<<endl;
+    return max(leftanswer , rightanswer)  ; 
+}
+    vector<bool> getResults(vector<vector<int>>& queries) {
+        vector<bool> temp;
+        int n = 1e5;
+ 
+        Segtree.resize(3*n,0);  
+       
+        set<int> st;
+        st.insert(0);
+        st.insert(n);
+        pointupdate(0,n-1,0,0,n);
+ 
+      
+        for(auto it:queries) {
+            int type=it[0];
+            if(type==1) { 
+               int x= it[1];
+               auto it1 = st.lower_bound(x);
+               it1--;
+               auto it2 = st.upper_bound(x);
+
+               int l = *it1;
+               int r = *it2;
+               
+               pointupdate(0,n-1,0,l,x-l);
+               pointupdate(0,n-1,0,x,r-x);
+               st.insert(x);
+            } else {
+               int x = it[1], sz = it[2];
+               auto it1 = st.lower_bound(x);
+               it1--;
+
+               int l = *it1;
+               int ans = max(rangeMax(0,n-1,0,0,l-1), x-l);
+ 
+               temp.push_back(ans>=sz);
+            } 
+         
+        }
+
+        //0-1 1-99  1-10 11-89  1-3  4-7  4-4  8-3
+
+         return temp;
+    }
+};
