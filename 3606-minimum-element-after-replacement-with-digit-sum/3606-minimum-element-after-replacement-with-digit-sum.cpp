4169bfdfe427db8cd8026e47be594556b9e420bc
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-09-28 20:02:46 +0530

    Time: 4 ms (91.67%), Space: 27.2 MB (91.67%) - LeetHub

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,42 @@
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int n= nums.size();
+        int mini = INT_MAX;
+        for(auto it:nums) {
+            int sum=0;
+            while(it>0) {
+                sum+=it%10;
+                it/=10;
+            }
+            mini = min(mini, sum);
+        }
+
+        return mini;
+}
+};
