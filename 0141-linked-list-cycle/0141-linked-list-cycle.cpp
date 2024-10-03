247c1a379dd0188a6c34e1a1d293ac856f536f30
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-03 11:20:22 +0530

    Time: 12 ms (35.51%), Space: 10.9 MB (21.8%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-         unordered_map<ListNode*,int> mp;
-         ListNode* temp = head;
-         while(temp!=NULL) {
-            if(mp[temp]) {
-                return true;
-            }
-            mp[temp]++;
-            temp = temp->next;
-         }
-         return false;
+        // ---------- Brute force  tc-O(n*logn)  sc-O(n)
+        //  unordered_map<ListNode*,int> mp;
+        //  ListNode* temp = head;
+        //  while(temp!=NULL) {
+        //     if(mp[temp]) {
+        //         return true;
+        //     }
+        //     mp[temp]++;
+        //     temp = temp->next;
+        //  }
+        //  return false;
+
+        // ----------- Optimized Version   tc- O(n) sc-O(1)
+
+        ListNode* hare = head, *tortoise = head;
+
+        while(hare and hare->next and hare->next->next) {
+            hare = hare->next->next;
+            tortoise = tortoise->next;
+            
+             if(hare==tortoise) return true;
+        }
+        return false;
     }
 };
