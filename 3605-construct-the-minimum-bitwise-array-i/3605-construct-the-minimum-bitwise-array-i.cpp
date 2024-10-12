c14cd408b01699c90d56dede283bf5e5f4b6bcab
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-12 20:11:17 +0530

    Time: 4 ms (73.33%), Space: 24.4 MB (13.33%) - LeetHub

---
+++ 3605-construct-the-minimum-bitwise-array-i/3605-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,44 @@
+
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+#define N 1e5+1
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vi ans;
+         for(auto it:nums) {
+            int a=-1;
+             for(int mid=it-1;mid>=1;mid--) {
+
+                if( (mid | (mid+1)) == it ) {
+                    a = mid;
+                }
+             }
+             ans.pb(a);
+         }
+         return ans;
+    }
+};
