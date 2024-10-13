ade83d98ef5ff37013f41d1cee0d77ff27cf920a
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-13 15:58:38 +0530

    Time: 2076 ms (5.36%), Space: 300.4 MB (5.04%) - LeetHub

---
+++ 3583-sorted-gcd-pair-queries/3583-sorted-gcd-pair-queries.cpp
@@ -0,0 +1,73 @@
+
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+#define N 1e5+1
+
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        vector<long long> cntDiv(1e5+1),gcdCnt(1e5+1,0);
+        for(auto it:nums) {
+            for(int i=1;i*i<=it;i++) {
+                if(it%i==0) {
+                    cntDiv[i]++;
+                    if(i != (it/i)) {
+                        cntDiv[it/i]++;
+                    }
+                } 
+            }
+        }
+
+        for(int i=N-1;i>=1;i--) {
+            int cnt = cntDiv[i];
+            gcdCnt[i] = (cnt*1LL*(cnt-1))/2 ;
+            for(int j= i*2;j<=N-1;j+=i) {
+                gcdCnt[i] -= gcdCnt[j];
+            }
+        }
+
+        vector<long long> psum(N+1);
+        vi temp;
+        for(int i=1;i<=N-1;i++) {
+            psum[i] = psum[i-1] + gcdCnt[i];
+        }
+        
+       for(auto it:queries) {
+            int low=0,high=N*(N-1)/2,ans=-1;
+            while(low<=high) {
+                int mid = (low+high)>>1;
+
+                if(psum[mid]>it) {
+                    ans = mid;
+                    high = mid-1;
+                } else low = mid+1;
+            }
+            temp.push_back(ans);
+       }
+
+       return temp;
+
+    }
+};
