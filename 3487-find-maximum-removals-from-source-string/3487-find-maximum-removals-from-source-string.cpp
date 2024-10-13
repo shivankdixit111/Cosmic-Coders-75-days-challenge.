4d70f345fb251781a0bd953bc6cf1de8b01c6701
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-13 07:52:56 +0530

    Time: 395 ms (18.18%), Space: 94.6 MB (9.09%) - LeetHub

---
+++ 3487-find-maximum-removals-from-source-string/3487-find-maximum-removals-from-source-string.cpp
@@ -0,0 +1,63 @@
+
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+#define N 1e5+1
+
+class Solution {
+public:
+    umii mp;
+    vvii dp;
+    int n,m;
+    int rec(int i,int j,string &s,string &t) {
+        if(i==n) {
+            if(j==m) return 0;
+            return -1e9;
+        }
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int ans = rec(i+1,j,s,t);
+        if(mp.count(i)) {
+            ans = max(ans, 1 + rec(i+1,j,s,t));
+        }
+        if(s[i]==t[j]) {
+            ans = max(ans,rec(i+1,j+1,s,t));
+        }
+        
+        return dp[i][j] = ans;
+    }
+
+    int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
+        n = source.size();
+        m = pattern.size();
+
+        for(auto it:targetIndices) mp[it]++;
+
+        dp.resize(n+1,vi(m+1,-1));
+
+        return rec(0,0,source,pattern);
+         
+    }
+};
