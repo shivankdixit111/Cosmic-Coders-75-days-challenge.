5c88b951f03092cf8328f388b265f90ffa8b457c
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-17 16:12:53 +0530

    Time: 575 ms (5.01%), Space: 8.9 MB (55.81%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   int dp[22][22];
+    bool rec(int i,int j, string &s, string &t) {
+        if(j>=t.length()) {
+            return i==s.length();
+        }
+        bool check = false;
+        if(i<s.length() and s[i]==t[j] or (t[j]=='.')) check = true;
+
+        if(j<t.length()-1 and t[j+1]=='*') {
+            int nottake = rec(i,j+2,s,t);
+            int take = false; 
+            if(i<s.length()) take = check and rec(i+1,j,s,t);
+
+            return take | nottake;
+        }
+
+        return check and rec(i+1,j+1,s,t);
+    }
+     
+    bool isMatch(string s, string p) {
+         for(int i=0;i<22;i++) 
+            for(int j=0;j<22;j++) 
+               dp[i][j] = -1;
+         return rec(0,0,s,p);
+    }
+};
