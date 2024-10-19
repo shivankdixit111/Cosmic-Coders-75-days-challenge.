0f2411df13809c5309bbbfee433e0fb2756c2e60
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-19 19:25:35 +0530

    Time: 6 ms (99.93%), Space: 53.2 MB (11.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -17,15 +17,26 @@ public:
          int n = nums.size();
           vector<int> dp(n+1,0);
         //  return  rec(0,n,nums,dp);
-        dp[0] = true;
-        for(int i=0;i<n;i++) { 
-            if(dp[i]==false) continue;
-            for(int j=1;j<=nums[i];j++) { 
-                if( (i+j)<n and !dp[i+j] ) {
-                     dp[i+j] = true;  
-                }
-            } 
+
+        //------------ tabulation
+        // dp[0] = true;
+        // for(int i=0;i<n;i++) { 
+        //     if(dp[i]==false) continue;
+        //     for(int j=1;j<=nums[i];j++) { 
+        //         if( (i+j)<n and !dp[i+j] ) {
+        //              dp[i+j] = true;  
+        //         }
+        //     } 
+        // }
+        // return dp[n-1];
+
+
+        //-----------------greedy
+        int maxi = 0;
+        for(int i=0;i<n;i++) {
+              if(i>maxi) return false;
+           maxi = max(maxi,i+nums[i]);
         }
-        return dp[n-1];
+        return  true;
     }
 };
