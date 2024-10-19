27b4fa86a91ed545efb596ebc53e854c9a32c6fb
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-19 19:21:02 +0530

    Time: 1508 ms (5.01%), Space: 53.3 MB (8.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -15,7 +15,17 @@ public:
 
     bool canJump(vector<int>& nums) {
          int n = nums.size();
-          vector<int> dp(n+1,-1);
-         return  rec(0,n,nums,dp);
+          vector<int> dp(n+1,0);
+        //  return  rec(0,n,nums,dp);
+        dp[0] = true;
+        for(int i=0;i<n;i++) { 
+            if(dp[i]==false) continue;
+            for(int j=1;j<=nums[i];j++) { 
+                if( (i+j)<n and !dp[i+j] ) {
+                     dp[i+j] = true;  
+                }
+            } 
+        }
+        return dp[n-1];
     }
 };
