b73631cd3015fe62b875b8c0069561fbab882db5
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-20 11:37:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (62.08%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool rec(int i, int j,int k, int n,int m, string &s1, string &s2, string &s3,
-    vector<vector<vector<int>>> &dp) {
+    vector<vector<int>> &dp) {
          if(k==s3.length()){
             return (i==n and j==m);
          }
-         if(dp[i][j][k]!=-1) return dp[i][j][k];
+         if(dp[i][j]!=-1) return dp[i][j];
 
          bool ans = false;         
          if(i<n and s1[i]==s3[k]) {
@@ -14,12 +14,12 @@ public:
          if(j<m and s2[j]==s3[k]) {
             ans = ans | (rec(i,j+1,k+1,n,m,s1,s2,s3,dp) or rec(i,j+1,k,n,m,s1,s2,s3,dp));
          }
-         return dp[i][j][k] = ans;
+         return dp[i][j] = ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
          int n = s1.length(),m=s2.length();
          if(n+m != s3.length()) return false;
-         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(m+1,vector<int>(s3.length()+1,-1)));
+         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
          return rec(0,0,0,n,m,s1,s2,s3,dp);
     }
 };
