b53f9703bc88ae2192778ebd7634eb4c700238d2
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-20 07:34:45 +0530

    Time: 0 ms (100%), Space: 7.5 MB (47.79%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        // refer to this article
+        // http://www.cs.cmu.edu/afs/cs/academic/class/15750-s17/ScribeNotes/lecture7.pdf
+        vector<int> dp(n+1,0);
+        dp[0]=1; // bst with 0 node
+        dp[1]=1; // bst with 1 node
+        
+        for(int i=2;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+};
