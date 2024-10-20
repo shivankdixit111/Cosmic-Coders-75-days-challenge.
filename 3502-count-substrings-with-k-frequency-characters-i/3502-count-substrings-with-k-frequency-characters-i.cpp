c8abf4347d0fa62497937f924b5894e3bd8748e6
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-20 08:05:03 +0530

    Time: 75 ms (50%), Space: 17.7 MB (50%) - LeetHub

---
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
@@ -0,0 +1,49 @@
+// lc
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+#define rev(a) reverse(a.begin(), a.end());
+
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        int n=s.length();
+        int i=0,ans=0;
+       
+        rep(i,0,n) {
+            int j=i;
+             vi mp(26,0);
+             while(j<n) {
+                 mp[s[j]-'a']++;
+                 if(mp[s[j]-'a']==k) {
+                    ans += n-j;
+                    break;
+                 }
+                 j++;
+             }
+            //  cout<<i<<": "<<ans<<endl; 
+        }
+        return ans;
+    }
+};
