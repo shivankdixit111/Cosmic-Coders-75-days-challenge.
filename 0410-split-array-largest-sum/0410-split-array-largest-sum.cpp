47a0ee335bd503e13e7529dc59be3f9f08e547ab
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-10-24 08:52:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (88.84%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,59 @@
+// lc
+#define vi vector<int>
+#define vvii vector<vi>
+#define vpii vector<pair<int,int>>
+#define vs vector<string>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define minpq(a) priority_queue<a,vi,greater<a>> pq
+#define maxpq(a) priority_queue<a> pq 
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define pb push_back 
+#define in insert
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) x.begin(), x.end()
+#define sall(x) sort(x.begin(), x.end());
+#define rall(x) sort(x.rbegin(), x.rend())
+#define MAX(x) *max_element(x.begin(), x.end())
+#define MIN(x) *min_element(x.begin(), x.end())
+#define SUM(X) accumulate(X.begin(), X.end(), 0LL)
+#define rev(a) reverse(a.begin(), a.end());
+
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = 0, high = SUM(nums) , ans=-1;
+        auto check = [&](int mid) {
+             int cnt=1,sum=0;
+             for(int i=0;i<n;i++) {
+                 if( nums[i] > mid ) return false;  //not possible
+                 if(sum + nums[i] <= mid) {
+                    sum += nums[i];
+                 } else {
+                    sum = nums[i];
+                    cnt++;
+                 }
+             }
+             return cnt<=k;
+        };
+
+        while(low<=high) {
+            int mid = (low+high)>>1;
+
+            if(check(mid)) { 
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
