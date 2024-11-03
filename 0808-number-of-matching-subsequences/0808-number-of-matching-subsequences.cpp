18bd1d06c7f8f2be0b6a97832385c7a5533b32ef
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2024-11-03 16:38:33 +0530

    Time: 507 ms (17.18%), Space: 334.6 MB (17.23%) - LeetHub

--- 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.cpp
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.cpp
@@ -1,17 +1,28 @@
+struct Node{
+    string word;
+    int index;
+    Node(string &word, int index) :word(word),index(index) {}
+};
+
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        unordered_map<string,int> mp;
-        for(auto it:words) mp[it]++;
-        int cnt=0;
-        for(auto it:mp) {
-            int i=0,j=0;
-            while(i<s.length() and j<it.first.length()) {
-                if(it.first[j]==s[i]) i++,j++;
-                else i++;
-            }
-            cnt+= (j==it.first.length()) ? it.second : 0;
+        vector<Node> bucket[26];
+        for(auto word:words) {
+           bucket[(word[0]-'a')].push_back(Node(word,0));
         }
-        return cnt;
+        int ans=0;
+        for(auto it:s) {
+            auto x = bucket[it-'a']; 
+            bucket[it-'a'].clear();
+            for(auto &node:x) { 
+                node.index++;
+                if(node.index==node.word.length()) ans++;
+                else {
+                    bucket[node.word[node.index]-'a'].push_back(node);
+                }
+            }
+        }
+        return ans;
     }
 };
